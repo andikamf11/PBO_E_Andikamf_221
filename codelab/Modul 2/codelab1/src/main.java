46b6a5cda58455f0456ec76d3ae2f0131c22f37b
andikamf11 <andikamulya2006@gmail.com>
2025-05-05 00:25:45 +0700

    codelab

---
+++ codelab/Modul 2/codelab1/src/main.java
@@ -0,0 +1,23 @@
+class hewan {
+
+    String nama, jenis, suara;
+
+    public void tampilkaninfo(){
+        System.out.printf("nama:  " + nama );
+        System.out.println("jenis: " + jenis);
+        System.out.println("suara: " + suara);
+    }
+}
+
+public class main {
+    public static void main(String[] args) {
+        hewan hewan1 = new hewan();
+        hewan hewan2 = new hewan();
+
+        hewan1.nama = "kucing";
+        hewan1.jenis = "mamalia";
+        hewan1.suara = "rawr";
+
+        hewan2.nama = 
+    }
+}
